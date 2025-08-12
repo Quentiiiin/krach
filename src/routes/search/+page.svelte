e8644efaa84c76ebc4a1293dc68d4ae21c233d81
Quentin <74738158+Quentiiiin@users.noreply.github.com>
2025-08-12 09:09:42 +0200

    added infinite scroll to search results

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -2,25 +2,48 @@
     import PlayButton from "$lib/components/PlayButton.svelte";
     import TrackEntry from "$lib/components/TrackEntry.svelte";
     import { getSearchResults } from "$lib/remote/sc.remote.js";
+    import { intersect, type IntersectDetail } from "@svelte-put/intersect";
+    import type { SoundcloudTrack } from "soundcloud.ts";
 
     const { data } = $props();
 
-    /*
-    await getSearchResults({
-        term: data.term,
-        offset: (await data.results).length,
+    let newResults: SoundcloudTrack[] = $state([]);
+
+    const allResults = $derived.by(() => {
+        const firstResults = data.results.current;
+        if (firstResults) {
+            return [...data.results.current, ...newResults];
+        } else return [...newResults];
     });
-    */
+
+    async function onIntersect(event: CustomEvent<IntersectDetail>) {
+        const { observer, entries, direction } = event.detail;
+        if (direction === "down") {
+            const results = await getSearchResults({
+                term: data.term,
+                offset: allResults.length,
+            });
+            newResults = [...newResults, ...results];
+        }
+    }
 </script>
 
 {#await data.results}
     loading...
-{:then results}
+{:then}
     <ul>
-        {#each results as result}
+        {#each allResults as result, index}
             <li class="m-2">
                 <TrackEntry track={result} />
+                {#if index % 5 === 0 && index > 1}
+                    <div
+                        class=" border border-amber-400"
+                        use:intersect
+                        onintersectonce={onIntersect}
+                    ></div>
+                {/if}
             </li>
         {/each}
     </ul>
+    <div use:intersect onintersect={onIntersect}>loading...</div>
 {/await}
