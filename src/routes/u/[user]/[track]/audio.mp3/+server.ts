58e1a3cc6a1f45cb4587dc7d27ee4b29f4b1d299
Quentin <74738158+Quentiiiin@users.noreply.github.com>
2025-08-12 10:01:14 +0200

    a

--- src/routes/u/[user]/[track]/audio.mp3/+server.ts
+++ src/routes/u/[user]/[track]/audio.mp3/+server.ts
@@ -1,5 +1,10 @@
 import { getAudio, streamToArrayBuffer } from '$lib/server/sc-util';
 import type { RequestHandler } from './$types';
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+    runtime: 'nodejs22.x'
+};
 
 export const GET: RequestHandler = async ({ params }) => {
     const stream = await getAudio(params.user, params.track);
