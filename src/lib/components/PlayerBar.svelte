9b3a5cfa80e7e04b865646d3e7f4bcb8dc9d99b1
Quentin <74738158+Quentiiiin@users.noreply.github.com>
2025-08-13 12:49:43 +0200

    fixed layout

--- src/lib/components/PlayerBar.svelte
+++ src/lib/components/PlayerBar.svelte
@@ -17,71 +17,35 @@
     );
 </script>
 
-<div
-    class="fixed z-10 bottom-0 left-0 w-screen bg-fuchsia-400 flex md:bottom-4 md:left-1/2 md:-translate-x-1/2 md:w-fit md:rounded-md md:border-2 border-t-2 border-black px-3 md:py-2 items-center space-x-4 scale-90 md:scale-100"
->
-    <PlayButton />
-    <div class="flex flex-col items-center w-full">
-        <span
-            class="mb-1 whitespace-nowrap text-base overflow-hidden text-ellipsis max-w-56"
-        >
-            {audioPlayer.currentTrack?.title ?? "No track playing"}
-        </span>
-        <input
-            type="range"
-            class="range-slider track-progress-slider w-full bg-white h-2 rounded-xl border border-black mb-2"
-            min="0"
-            max={(audioPlayer.currentTrack?.full_duration ?? 0) / 1000}
-            bind:value={audioPlayer.currentTime}
-            step="0.1"
-            aria-label="Track Progress"
-        />
-        <div class="flex justify-between w-full text-xs text-gray-800">
-            <span>{formattedCurrentTime}</span>
-            <span>{formattedTotalDuration}</span>
+<div class=" flex fixed z-10 bottom-0 left-0 justify-center w-screen">
+    <div
+        class=" md:mb-2 w-screen bg-fuchsia-400 flex md:w-fit md:rounded-md md:border-2 border-t-2 border-black px-3 md:py-2 items-center space-x-4"
+    >
+        <PlayButton />
+        <div class="flex flex-col items-center w-full">
+            <span
+                class="mb-1 whitespace-nowrap text-base overflow-hidden text-ellipsis max-w-56"
+            >
+                {audioPlayer.currentTrack?.title ?? "No track playing"}
+            </span>
+            <input
+                type="range"
+                class="range-slider track-progress-slider w-full bg-white h-2 rounded-xl border border-black mb-2"
+                min="0"
+                max={(audioPlayer.currentTrack?.full_duration ?? 0) / 1000}
+                bind:value={audioPlayer.currentTime}
+                step="0.1"
+                aria-label="Track Progress"
+            />
+            <div class="flex justify-between w-full text-xs text-gray-800">
+                <span>{formattedCurrentTime}</span>
+                <span>{formattedTotalDuration}</span>
+            </div>
         </div>
+
+        <Thumbnail
+            classNames="md:h-16 h-14 flex-shrink-0 rounded-md"
+            src={audioPlayer.currentTrack?.artwork_url}
+        />
     </div>
-
-    <Thumbnail
-        classNames="md:h-16 h-14 flex-shrink-0 rounded-md"
-        src={audioPlayer.currentTrack?.artwork_url}
-    />
 </div>
-
-<style lang="postcss">
-    @reference '$lib/../app.css'
-
-    .range-slider {
-        -webkit-appearance: none;
-        appearance: none;
-        background: transparent;
-
-        @apply border-2 border-black rounded-2xl;
-    }
-
-    .range-slider::-moz-range-thumb,
-    .range-slider::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        appearance: none;
-        @apply h-3.5 w-3.5 rounded-full shadow-md border-2 border-black;
-    }
-
-    .range-slider::-webkit-slider-runnable-track {
-        @apply h-2 rounded-lg;
-    }
-
-    .range-slider::-moz-range-track {
-        @apply h-2 rounded-lg;
-    }
-
-    .range-slider::-ms-track {
-        @apply h-2 rounded-lg;
-        border-color: transparent;
-        color: transparent;
-    }
-
-    .range-slider::-ms-fill-lower,
-    .range-slider::-ms-fill-upper {
-        background: transparent;
-    }
-</style>
